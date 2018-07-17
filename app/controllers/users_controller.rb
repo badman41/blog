ca8152fc6bd5d258a79cbf350f8bd5fcc16c9007
TuanDV <dangtuannoone@gmail.com>
2018-07-17 07:41:04 +0700

    User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :find_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.all_user.all_order
+                 .paginate(page: params[:page], per_page: Settings.per_page)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    #@microposts = @user.microposts.paginate(page: params[:page])
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t ".flash_activate"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".flash_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".flash"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:notice] = t "users.find_user.flash"
+    redirect_to users_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   scope "(:locale)", :locale => /en|vn/ do
     root "static_pages#home"
     get "/home", to: "static_pages#home"
     get "/about", to: "static_pages#help", as: "about"
+    get  '/signup',  to: 'users#new'
+    get 'sessions/new'
+    get 'users/new'
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "password_resets/new"
+    get "password_resets/edit"
+    resources :users
+    resources :account_activations, only: [:edit]
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
 end
