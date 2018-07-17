71c990448caffdf6fcc184824c82162c7e67485b
TuanDV <dangtuannoone@gmail.com>
2018-07-17 08:43:18 +0700

    UserFollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
   scope "(:locale)", :locale => /en|vn/ do
     root "static_pages#home"
     get "/home", to: "static_pages#home"
@@ -8,13 +6,19 @@ Rails.application.routes.draw do
     get  '/signup',  to: 'users#new'
     get 'sessions/new'
     get 'users/new'
+    post "/signup", to: "users#create"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     get "password_resets/new"
     get "password_resets/edit"
+    get 'follow/following'
+    get 'follow/follower'
+    get "following/:id", to:"follow#following", as: "following"
+    get "followers/:id", to:"follow#followers", as: "followers"
     resources :users
     resources :account_activations, only: [:edit]
     resources :password_resets, only: [:new, :create, :edit, :update]
+    resources :relationships, only: [:create, :destroy]
   end
 end
